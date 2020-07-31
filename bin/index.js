b2ae1d375b072ab3e08a9c3d197c0e0856208a5c
Paul Carleton <paulcarletonjr@gmail.com>
2020-07-31 08:25:24 -0700

    Error without argument

--- bin/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { parseSpreadsheet } from "../src/parse.js";
-
-parseSpreadsheet(process.argv[2]);
---
+++ src/model.js
@@ -0,0 +1,3 @@
+export function modelPaycheck(dummy) {
+  return "Hello from model.js!!";
+}
