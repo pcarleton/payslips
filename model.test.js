3bb6d32ce995422ba155a43b95bc7b0c57f29e2b
Paul Carleton <paulcarletonjr@gmail.com>
2020-08-09 14:28:15 -0700

    lint test file

--- model.test.js
+++ model.test.js
@@ -1,15 +1,14 @@
 import { modelPaycheck } from "./src/model.js";
 import TaxCalculator from "./src/tax_calculator.js";
-import { jest } from '@jest/globals';
 
 test("calculates a simple tax", () => {
-  expect(TaxCalculator.calculateTax(100, [{value: 0, rate: 0.1}])).toBe(10);
+  expect(TaxCalculator.calculateTax(100, [{ value: 0, rate: 0.1 }])).toBe(10);
 });
 
 test("calculates a progressive tax", () => {
   const table = [
-    {value: 0, rate: 0.1},
-    {value: 1000, rate: 0.2},
+    { value: 0, rate: 0.1 },
+    { value: 1000, rate: 0.2 },
   ];
   expect(TaxCalculator.calculateTax(100, table)).toBe(10);
   expect(TaxCalculator.calculateTax(1000, table)).toBe(100);
@@ -17,11 +16,13 @@ test("calculates a progressive tax", () => {
 });
 
 test("creates a simple table", () => {
-  const rows = [[0, 0.1], [1000, 0.2]]
+  const rows = [
+    [0, 0.1],
+    [1000, 0.2],
+  ];
   const table = [
-    {start: 0, end: 1000, rate: 0.1},
-    {start: 1000, end: undefined, rate: 0.2},
+    { start: 0, end: 1000, rate: 0.1 },
+    { start: 1000, end: undefined, rate: 0.2 },
   ];
   expect(TaxCalculator.makeTable(rows)).toStrictEqual(table);
 });
-
