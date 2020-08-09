6141a281b33d46a24609cd4907aa41a0594432c7
Paul Carleton <paulcarletonjr@gmail.com>
2020-08-09 09:41:04 -0700

    Add progressive tax calculation

--- model.test.js
+++ model.test.js
@@ -3,6 +3,16 @@ import TaxCalculator from "./src/tax_calculator.js";
 import { jest } from '@jest/globals';
 
 test("calculates a simple tax", () => {
-  expect(TaxCalculator.calculateTax(100, [{rate: 0.1}])).toBe(10);
+  expect(TaxCalculator.calculateTax(100, [{value: 0, rate: 0.1}])).toBe(10);
+});
+
+test("calculates a progressive tax", () => {
+  const table = [
+    {value: 0, rate: 0.1},
+    {value: 1000, rate: 0.2},
+  ];
+  expect(TaxCalculator.calculateTax(100, table)).toBe(10);
+  expect(TaxCalculator.calculateTax(1000, table)).toBe(100);
+  expect(TaxCalculator.calculateTax(1100, table)).toBe(120);
 });
 
--- src/tax_calculator.js
+++ src/tax_calculator.js
@@ -1,10 +1,18 @@
-
 const TaxCalculator = {
   calculateTax: (sum, table) => {
-    return sum * table[0].rate;
-  }
+    let taxDue = 0;
+    let unTaxed = sum;
+    for (let i = table.length - 1; i >= 0; i -= 1) {
+      const row = table[i];
+      // Get the amount of untaxed income above this value
+      const applicable = Math.max(0, unTaxed - row.value);
 
+      taxDue += applicable * row.rate;
+      unTaxed -= applicable;
+    }
 
-}
+    return taxDue;
+  },
+};
 
 export default TaxCalculator;
